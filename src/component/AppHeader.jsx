c27b539c02d62e081a178975ca7ff0e44938ccfb
Hiukhaw <priyaphattvt@gmail.com>
2025-11-12 16:43:02 +0700

    inital commit

---
+++ src/component/AppHeader.jsx
@@ -0,0 +1,12 @@
+const AppHeader = () => {
+    return ( 
+        <>  
+        <div className="text-center mt-3 mb-3 bg-success text-white p-2 rounded-3">
+        <h1>CSI205-FRONT-END SOFTWARE DEVELOPMENT</h1>
+
+        </div>
+        </>
+     )
+}
+ 
+export default AppHeader;
